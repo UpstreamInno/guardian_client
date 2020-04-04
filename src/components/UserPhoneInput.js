49b8fce1521a5060d9462c4729a4d9c5a5a1f933
kvey <colton@coltonpierson.com>
2020-04-04 03:17:51 -0700

    Phone number input screen

--- src/components/UserPhoneInput.js
+++ src/components/UserPhoneInput.js
@@ -1,22 +1,41 @@
 import React from "react";
 import {
+  View,
   Text,
-  TextInput
+  TextInput,
+  StyleSheet,
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "start"
+  },
+  text: {
+    color: "#FFF",
+    textAlign: "left",
+    fontSize: '20px',
+    marginLeft: "10px"
+  },
+  phoneInput: {
+    height: '40px',
+    fontSize: '30px',
+    borderColor: 'gray',
+    borderWidth: '2px',
+    backgroundColor: "#FFFFFF",
+    borderRadius: '10px',
+    margin: '3px'
+  },
+});
 
-export const UserPhoneInput = ({ onChange, phoneNumber }) => {
-
+export const UserPhoneInput = ({ onChange, phoneNumber, style }) => {
   return (
-    <>
-      <Text>User Phone:</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Phone Number</Text>
       <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        style={styles.phoneInput}
         onChangeText={onChange}
         value={phoneNumber}
-      />
-      <Text>User phone from store:</Text>
-      <Text>{phoneNumber}</Text>
-    </>
+        />
+    </View>
   );
 };
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,38 +1,76 @@
 import React from "react";
 import {
   StyleSheet,
-  View
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
 } from "react-native";
-
+import Constants from "expo-constants";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserPhone } from "Store/actions";
 
+import { LinearGradient } from "expo-linear-gradient";
 import { UserPhoneInput } from "Components/UserPhoneInput"
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 10,
+    color: "#ffffff"
   },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1"
+  },
+  signUpButton: {
+    color: "#FFFFFF",
+    fontSize: "20px",
+    textAlign: "center",
+  },
+  centerLogo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
 
 const ProfileScreen = () => {
   const dispatch = useDispatch();
 
-  const {
-    userPhone
-  } = useSelector(state => state);
+  const { userPhone } = useSelector(state => state);
 
   const onSetUserPhone = phoneNumber => {
     dispatch(setUserPhone(phoneNumber));
   };
 
+  const onSubmit = () => {
+    /* dispatch({}); */
+  };
+
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={["#94e4f9", "#2d93d8"]} style={styles.container}>
+      <View styles={styles.centerLogo}>
+        <Text style={styles.title}>Guardian</Text>
+        <Image
+          source={require("../../media/logo.png")}
+          style={{ width: 200, height: 200, marginBottom: 50 }}
+        />
+      </View>
       <UserPhoneInput
+        style={styles.phoneInput}
         onChange={onSetUserPhone}
         phoneNumber={userPhone}
       />
-    </View>
+      <TouchableOpacity onPress={onSubmit}>
+        <Text style={styles.signUpButton}>Sign Up</Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 };
 
