ff1219cddf930ebe9ee7c63e89289613e7f14c80
Ash West <ashlandmwest@gmail.com>
2020-04-09 10:47:05 -0700

    First cherry-pick

--- src/components/SymptomSurveyScreen.js
+++ src/components/SymptomSurveyScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { t } from "Lib/i18n";
 import { routeTo } from "Store/actions";
-import { Pages } from "Lib/Pages";
+import { Pages } from "Components/GuardianContainer";
 import {
+  ScrollView,
   StyleSheet,
   Switch,
   Text,
@@ -10,69 +11,103 @@ import {
   View,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { useDispatch } from "react-redux";
 
-const TRACK_COLOR = { false: "#696969", true: "#32cd32" };
-const THUMB_COLOR = "#fff";
-const IOS_SWITCH_BG_COLOR = "#696969";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: "20px",
+    backgroundColor: "#fff",
+    paddingLeft: "30px",
+  },
+  switchContainer: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: "20px",
+    paddingLeft: "30px",
+  },
+  submitButton: {
+    width: "200px",
+    height: "50px",
+    backgroundColor: "#FFFFFF",
+    color: "#000000",
+    paddingTop: "15px",
+    paddingBottom: "15px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    borderRadius: "10px",
+  },
+  submitButtonText: {
+    color: "#000000",
+    fontSize: "14px",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "20px",
+    textAlign: "left",
+    margin: 10,
+    color: "#ffffff",
+  },
+  label: {
+    fontSize: "18px",
+    color: "#FFFFFF",
+    marginLeft: "10px",
+  },
+  row: {
+    flex: "0",
+    flexDirection: "row",
+    marginBottom: "20px",
+  },
+  submitContainer: {
+    flex: "1",
+    paddingBottom: "20px",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
 
-const SymptomSurveyScreen = () => {
-  const dispatch = useDispatch();
+const TRACK_COLOR = { false: "#FFF", true: "#81b0ff" };
+const THUMB_COLOR = "#f5dd4b";
+const IOS_SWITCH_BG_COLOR = "#3e3e3e";
 
+const ReportScreen = () => {
   const [symptoms, setSymptoms] = React.useState({});
 
-  const onSubmit = () => {
-    console.log("reported symptoms", symptoms)
-    dispatch(routeTo(Pages.SURVEY_COMPLETE));
-  };
+  const onSubmit = () => {};
 
-  const SwitchRow = ({ symptom, userFriendlyText }) => {
-    return (
-      <View style={styles.row}>
-        <Switch
-          trackColor={TRACK_COLOR}
-          thumbColor={THUMB_COLOR}
-          //I left the IOS background color here becasue I do not have an iphone to test this on, but I think it may be redundant given the style
-          ios_backgroundColor={IOS_SWITCH_BG_COLOR}
-          onValueChange={(v) => setSymptoms({ ...symptoms, [symptom]: v })}
-          value={symptoms[symptom]}
-          style={{ transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
-        />
-        <Text style={styles.label}>{t(userFriendlyText)}</Text>
-      </View>
-    );
+  const toggleSwitch = () => {};
+
+  const SwitchRow = ({ symptom }) => {
+    <View style={styles.row}>
+      <Switch
+        trackColor={TRACK_COLOR}
+        thumbColor={THUMB_COLOR}
+        ios_backgroundColor={IOS_SWITCH_BG_COLOR}
+        onValueChange={(v) => setSymptoms({ ...symptoms, [symptom]: v })}
+        value={symptoms[symptom]}
+      />
+      <Text style={styles.label}>Loss of Sense of Smell</Text>
+    </View>;
   };
 
   return (
-    <LinearGradient colors={["#94e4f9", "#2d93d8"]} style={styles.container}>
+    <LinearGradient colors={["#000", "#000"]} style={styles.container}>
       <Text style={styles.title}>
-        {t("Please check any of the following symptoms you have experianced in the last 5 days")}
+        Since the exposure, have you felt any of the following?
       </Text>
       <View style={styles.switchContainer}>
-        <SwitchRow
-          symptom="lossOfSenseOfSmell"
-          userFriendlyText="Loss of Sense of Smell"
-        />
-        <SwitchRow symptom="cough" userFriendlyText="Cough" />
-        <SwitchRow symptom="fever" userFriendlyText="Fever" />
-        <SwitchRow symptom="tiredness" userFriendlyText="Tiredness" />
-        <SwitchRow
-          symptom="difficultyBreathing"
-          userFriendlyText="Difficulty Breathing"
-        />
-        <SwitchRow
-          symptom="pressureInChest"
-          userFriendlyText="Pressure in Chest"
-        />
-        <SwitchRow
-          symptom="testedPositive"
-          userFriendlyText="I Have Tested Positive for COVID-19"
-        />
+        <SwitchRow symptom="lossOfSenseOfSmell" />
+        <SwitchRow symptom="cough" />
+        <SwitchRow symptom="fever" />
+        <SwitchRow symptom="tiredness" />
+        <SwitchRow symptom="difficultyBreathing" />
+        <SwitchRow symptom="pressureInChest" />
+        <SwitchRow symptom="testedPositive" />
       </View>
       <View style={styles.submitContainer}>
         <TouchableOpacity onPress={onSubmit}>
           <View style={styles.submitButton}>
-            <Text style={styles.submitButtonText}>Submit</Text>
+            <Text style={styles.submitButtonText}>Submit your symtoms</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -80,69 +115,8 @@ const SymptomSurveyScreen = () => {
   );
 };
 
+ReportScreen.navigationOptions = {
+  title: "Report",
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: "#fff",
-    // paddingLeft: 30,
-  },
-  switchContainer: {
-    flex: 2,
-    justifyContent: "center",
-    paddingTop: 20,
-    paddingLeft: 40,
-  },
-  submitButton: {
-    width: 200,
-    height: 50,
-    backgroundColor: "#FFFFFF",
-    // color: "#000000",
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 20,
-    paddingRight: 20,
-    borderRadius: 10,
-  },
-  submitButtonText: {
-    color: "#000000",
-    fontSize: 14,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  title: {
-    fontSize: 25,
-    textAlign: "center",
-    margin: 40,
-    marginLeft: 10,
-    marginRight: 10,
-    color: "#ffffff",
-  },
-  label: {
-    fontSize: 18,
-    color: "#FFFFFF",
-    marginLeft: 10,
-    flex: 1,
-    flexWrap: "wrap",
-  },
-  row: {
-    flexDirection: "row",
-    marginBottom: 20,
-  },
-  submitContainer: {
-    flex: 1,
-    paddingBottom: 20,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  switchBorder: {
-    backgroundColor: "#3e3e3e",
-    borderRadius: 17,
-  },
-  switchBorderOff: {
-    backgroundColor: null,
-  },
-});
-
-export default SymptomSurveyScreen;
+export default ReportScreen;
