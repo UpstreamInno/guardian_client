f782d55ec0bc808c1e69619b56cb427228f00c87
Ash West <ashlandmwest@gmail.com>
2020-04-09 11:08:12 -0700

    Added Report Screen to Container

--- src/components/GuardianContainer.js
+++ src/components/GuardianContainer.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Text, Button } from 'react-native';
+import { Text, Button } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 import { routeTo } from "Store/actions";
 import DebugMenu from "Components/DebugMenu";
-import HomeScreen from "Components/HomeScreen"
-import OtpScreen from "Components/OtpScreen"
-import SignupScreen from "Components/SignupScreen"
+import HomeScreen from "Components/HomeScreen";
+import OtpScreen from "Components/OtpScreen";
+import SignupScreen from "Components/SignupScreen";
 import LocationScreen from "Components/LocationScreen";
 import AlertScreen from "Components/AlertScreen";
+import ReportScreen from "Components/SymptomSurveyScreen.js";
 
 export const Pages = {
   SIGNUP: "SIGNUP",
@@ -24,24 +25,24 @@ export const Pages = {
 
   // TODO: hide these debug pages in release builds
   DEBUG_MENU: "DEBUG_MENU",
-}
+};
 
 export default function GuardianContainer() {
-  const { currentPage } = useSelector(state => state);
+  const { currentPage } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   // TODO: remove this when other pages are complete!
   const placeholder = (page) => {
     return (
       <>
-        <Text>Placeholder for {page} screen</ Text>
+        <Text>Placeholder for {page} screen</Text>
         <Button
-            title="Go back to Debug"
-            onPress={() => dispatch(routeTo(Pages.DEBUG_MENU))}
+          title="Go back to Debug"
+          onPress={() => dispatch(routeTo(Pages.DEBUG_MENU))}
         />
       </>
     );
-  }
+  };
 
   switch (currentPage) {
     case Pages.HOME:
@@ -58,6 +59,7 @@ export default function GuardianContainer() {
     case Pages.CONSENT_NOTIFICATION:
     case Pages.MESSAGE_DETAILS:
     case Pages.SYMPTOM_SURVEY:
+      return <ReportScreen />;
     case Pages.SURVEY_COMPLETE:
       return placeholder(currentPage);
     case Pages.DEBUG_MENU:
@@ -65,4 +67,4 @@ export default function GuardianContainer() {
     default:
       return <DebugMenu />;
   }
-};
+}
