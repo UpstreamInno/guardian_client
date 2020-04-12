81bc315f9bd8825a822332b2c9b91bf3b0835038
Ash West <ashlandmwest@gmail.com>
2020-04-12 07:16:23 -0700

    Formatting changes

--- src/components/SymptomSurveyScreen.js
+++ src/components/SymptomSurveyScreen.js
@@ -15,67 +15,75 @@ import { LinearGradient } from "expo-linear-gradient";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: "20px",
+    paddingTop: 20,
     backgroundColor: "#fff",
-    paddingLeft: "30px",
+    // paddingLeft: 30,
   },
   switchContainer: {
-    flex: 1,
+    flex: 2,
     justifyContent: "center",
-    paddingTop: "20px",
-    paddingLeft: "30px",
+    paddingTop: 20,
+    paddingLeft: 40,
   },
   submitButton: {
-    width: "200px",
-    height: "50px",
+    width: 200,
+    height: 50,
     backgroundColor: "#FFFFFF",
     color: "#000000",
-    paddingTop: "15px",
-    paddingBottom: "15px",
-    paddingLeft: "20px",
-    paddingRight: "20px",
-    borderRadius: "10px",
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 10,
   },
   submitButtonText: {
     color: "#000000",
-    fontSize: "14px",
+    fontSize: 14,
     fontWeight: "bold",
     textAlign: "center",
   },
   title: {
-    fontSize: "20px",
-    textAlign: "left",
-    margin: 10,
+    fontSize: 25,
+    textAlign: "center",
+    margin: 40,
+    marginLeft: 10,
+    marginRight: 10,
     color: "#ffffff",
   },
   label: {
-    fontSize: "18px",
+    fontSize: 18,
     color: "#FFFFFF",
-    marginLeft: "10px",
+    marginLeft: 10,
+    flex: 1,
+    flexWrap: "wrap",
   },
   row: {
-    flex: "0",
     flexDirection: "row",
-    marginBottom: "20px",
+    marginBottom: 20,
   },
   submitContainer: {
-    flex: "1",
-    paddingBottom: "20px",
+    flex: 1,
+    paddingBottom: 20,
     justifyContent: "flex-end",
     alignItems: "center",
   },
+  switchBorder: {
+    backgroundColor: "#3e3e3e",
+    borderRadius: 17,
+  },
+  switchBorderOff: {
+    backgroundColor: null,
+  },
 });
 
-const TRACK_COLOR = { false: "#FFF", true: "#81b0ff" };
-const THUMB_COLOR = "#f5dd4b";
-const IOS_SWITCH_BG_COLOR = "#3e3e3e";
+const TRACK_COLOR = { false: "#696969", true: "#32cd32" };
+const THUMB_COLOR = "#fff";
+const IOS_SWITCH_BG_COLOR = "#696969";
 
 const ReportScreen = () => {
   const [symptoms, setSymptoms] = React.useState({});
 
   const onSubmit = () => {};
-
-  const toggleSwitch = () => {};
 
   const SwitchRow = ({ symptom, userFriendlyText }) => {
     return (
@@ -83,9 +91,11 @@ const ReportScreen = () => {
         <Switch
           trackColor={TRACK_COLOR}
           thumbColor={THUMB_COLOR}
+          //I left the IOS background color here becasue I do not have an iphone to test this on, but I think it may be redundant given the style
           ios_backgroundColor={IOS_SWITCH_BG_COLOR}
           onValueChange={(v) => setSymptoms({ ...symptoms, [symptom]: v })}
           value={symptoms[symptom]}
+          style={{ transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
         />
         <Text style={styles.label}>{userFriendlyText}</Text>
       </View>
@@ -95,7 +105,8 @@ const ReportScreen = () => {
   return (
     <LinearGradient colors={["#94e4f9", "#2d93d8"]} style={styles.container}>
       <Text style={styles.title}>
-        Since the exposure, have you felt any of the following?
+        Please check any of the following symptoms you have experianced since
+        your exposure
       </Text>
       <View style={styles.switchContainer}>
         <SwitchRow
@@ -115,13 +126,13 @@ const ReportScreen = () => {
         />
         <SwitchRow
           symptom="testedPositive"
-          userFriendlyText="Tested Positive"
+          userFriendlyText="I Have Tested Positive for COVID-19"
         />
       </View>
       <View style={styles.submitContainer}>
         <TouchableOpacity onPress={onSubmit}>
           <View style={styles.submitButton}>
-            <Text style={styles.submitButtonText}>Submit your symtoms</Text>
+            <Text style={styles.submitButtonText}>Submit</Text>
           </View>
         </TouchableOpacity>
       </View>
